1fc30e92afd971d86f6ff3b2fd6d42a6532ad411
Samar <samarjomysujith606@gmail.com>
2025-09-13 12:32:31 +0530

    Before removing collision

--- Player/player2.gd
+++ Player/player2.gd
@@ -1,28 +1,28 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var animation:AnimationPlayer=$"../AnimationPlayer"
-
+@onready var animation: AnimationPlayer = $"../AnimationPlayer"
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction:Vector2=Vector2.ZERO
-	direction.x=Input.get_action_strength("right")-Input.get_action_strength("left")
-	direction.y=Input.get_action_strength("down")-Input.get_action_strength("up")	
+	# Movement input
+	var direction: Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+
+	# Apply horizontal movement
+	velocity.x = direction.x * SPEED
+
+	# Apply movement each frame
+	move_and_slide()
 
 func _ready() -> void:
 	animation.play("idle")
-
-	move_and_slide()
