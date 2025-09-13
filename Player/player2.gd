b90e2ac14e0813b83a750663ae161125185bf339
Samar <samarjomysujith606@gmail.com>
2025-09-13 12:09:25 +0530

    Seconf

--- Player/player2.gd
+++ Player/player2.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animation:AnimationPlayer=$"../AnimationPlayer"
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +13,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction:Vector2=Vector2.ZERO
+	direction.x=Input.get_action_strength("right")-Input.get_action_strength("left")
+	direction.y=Input.get_action_strength("down")-Input.get_action_strength("up")	
+
+func _ready() -> void:
+	animation.play("idle")
 
 	move_and_slide()
